d555964f6bb8334b89870e8466365dc19cf13966
Eric Chanudet <chanudete@ainfosec.com>
2020-03-24 19:30:15 -0400

    doverifysign: Remove deprecated drivers
    xenvbd and scsifilt are no longer built, remove from the list.
    
    Signed-off-by: Eric Chanudet <chanudete@ainfosec.com>

--- doverifysign.ps1
+++ doverifysign.ps1
@@ -7,11 +7,11 @@ Import-Module $ScriptDir\..\BuildSupport\invoke.psm1
 
 Write-Host "signtool $signtool"
 Push-Location sign32
-Invoke-CommandChecked "Check 32 bit CAT files" $signtool verify /pa /v xennet.cat xenwnet.cat xenvbd.cat xenvesa-xp.cat xenvesa-lh.cat xenv4v.cat xevtchn.cat xeninp.cat xenaud.cat
-Invoke-CommandChecked "Check 32 bit boot start drivers" $signtool verify /pa /v xevtchn.sys xenv4v.sys xenvbd.sys xenvesa-miniport.sys xenvesa-display.dll scsifilt.sys xennet.sys xennet6.sys xenwnet.sys xenwnet6.sys xenutil.sys xeninp.sys xenaud.sys
+Invoke-CommandChecked "Check 32 bit CAT files" $signtool verify /pa /v xennet.cat xenwnet.cat xenvesa-xp.cat xenvesa-lh.cat xenv4v.cat xevtchn.cat xeninp.cat xenaud.cat
+Invoke-CommandChecked "Check 32 bit boot start drivers" $signtool verify /pa /v xevtchn.sys xenv4v.sys xenvesa-miniport.sys xenvesa-display.dll xennet.sys xennet6.sys xenwnet.sys xenwnet6.sys xenutil.sys xeninp.sys xenaud.sys
 Pop-Location
 
 Push-Location sign64
-Invoke-CommandChecked "Check 64 bit CAT files" $signtool verify /pa /v xennet.cat xenwnet.cat xenvbd.cat  xenvesa-xp.cat xenvesa-lh.cat xenv4v.cat xevtchn.cat xeninp.cat xenaud.cat
-Invoke-CommandChecked "Checked 64 bit boot start drivers" $signtool verify /pa /v xevtchn.sys xenv4v.sys xenvbd.sys xenvesa-miniport.sys xenvesa-display.dll scsifilt.sys xennet.sys xennet6.sys xenwnet.sys xenwnet6.sys xenutil.sys xeninp.sys xenaud.sys
+Invoke-CommandChecked "Check 64 bit CAT files" $signtool verify /pa /v xennet.cat xenwnet.cat xenvesa-xp.cat xenvesa-lh.cat xenv4v.cat xevtchn.cat xeninp.cat xenaud.cat
+Invoke-CommandChecked "Checked 64 bit boot start drivers" $signtool verify /pa /v xevtchn.sys xenv4v.sys xenvesa-miniport.sys xenvesa-display.dll xennet.sys xennet6.sys xenwnet.sys xenwnet6.sys xenutil.sys xeninp.sys xenaud.sys
 Pop-Location
